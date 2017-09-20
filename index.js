d5a1c692f3bc82dd0a7209002ed834c829ffd605
YauheniIhnatsyeu <tyz1@list.ru>
2017-09-20 20:54:46 +0300

    Task Complete

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const GuessingGame = require('./src/guessing-game.js');
 
+const game = new GuessingGame();
+
+game.setRange(0, 4048);
+
+let result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.lower();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.greater();
+result = game.guess();
+game.lower();
+result = game.guess();
+
+console.log(result);
+
+
+
+
 window.game = new GuessingGame();
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        //console.log(Math.floor((this.max + this.min) / 2));
+        return Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        //console.log('lower');
+        this.max = Math.round((this.max + this.min) / 2);
     }
 
     greater() {
-
+        //console.log('greater');
+        this.min = Math.round((this.max + this.min) / 2);
     }
 }
 
